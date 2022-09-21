b0cf8f22997eebd41731fe0b5ecdb4b499783c9e
Mi_Rolling <13422496263@163.com>
2022-09-21 18:33:01 +0800

    first commit

---
+++ module/staff/internal/service/staff.go
@@ -0,0 +1,122 @@
+package service
+
+import (
+	"context"
+	"github.com/go-kratos/kratos/v2/log"
+	"staff/api/staff/v1/generate"
+	"staff/constant"
+	"staff/internal/biz"
+
+	v1 "staff/api/staff/v1"
+)
+
+type StaffService struct {
+	v1.UnimplementedStaffServer
+
+	uc  *biz.StaffUsecase
+	log *log.Helper
+}
+
+func NewStaffService(uc *biz.StaffUsecase, logger log.Logger) *StaffService {
+	return &StaffService{uc: uc, log: log.NewHelper(logger)}
+}
+
+func (s *StaffService) CreateStaff(ctx context.Context, req *generate.CreateStaffRequest) (*generate.CreateStaffReply, error) {
+	_, err := s.uc.CreateStaff(ctx, &biz.Staff{
+		Mobile: req.Mobile,
+		Name:   req.Name,
+	})
+	if err != nil {
+		return nil, err
+	}
+	createStaffReply := generate.CreateStaffReply{
+		Exec: true,
+	}
+	return &createStaffReply, nil
+}
+
+func (s *StaffService) UpdateStaff(ctx context.Context, req *generate.UpdateStaffRequest) (*generate.UpdateStaffReply, error) {
+	_, err := s.uc.UpdateStaff(ctx, &biz.Staff{
+		ID:     req.Id,
+		Mobile: req.Mobile,
+		Name:   req.Name,
+	})
+	if err != nil {
+		return nil, err
+	}
+	updateStaffReply := generate.UpdateStaffReply{
+		Exec: true,
+	}
+	return &updateStaffReply, nil
+}
+
+func (s *StaffService) DeleteStaff(ctx context.Context, req *generate.DeleteStaffRequest) (*generate.DeleteStaffReply, error) {
+	_, err := s.uc.DeleteStaff(ctx, &biz.Staff{
+		ID:        req.Id,
+		IsDeleted: constant.True,
+	})
+	if err != nil {
+		return nil, err
+	}
+	deleteStaffReply := generate.DeleteStaffReply{
+		Exec: true,
+	}
+	return &deleteStaffReply, nil
+}
+
+func (s *StaffService) RecoveryStaff(ctx context.Context, req *generate.RecoveryStaffRequest) (*generate.RecoveryStaffReply, error) {
+	_, err := s.uc.DeleteStaff(ctx, &biz.Staff{
+		ID:        req.Id,
+		IsDeleted: constant.False,
+	})
+	if err != nil {
+		return nil, err
+	}
+	recoveryStaffReply := generate.RecoveryStaffReply{
+		Exec: true,
+	}
+	return &recoveryStaffReply, nil
+}
+
+func (s *StaffService) ListStaff(ctx context.Context, req *generate.ListStaffRequest) (*generate.ListStaffReply, error) {
+	// 类型转换 int -> bool
+	isDeleted := constant.False
+	if req.IsDeleted {
+		isDeleted = constant.True
+	}
+	list, total, err := s.uc.ListStaff(ctx, &biz.Staff{
+		Mobile:    req.Mobile,
+		Name:      req.Name,
+		IsDeleted: isDeleted,
+	}, int(req.Pn), int(req.PSize))
+	if err != nil {
+		return nil, err
+	}
+	rsp := &generate.ListStaffReply{}
+	rsp.Total = int32(total)
+	for _, staff := range list {
+		staffInfoRsp := generate.StaffReply{
+			Id:        staff.ID,
+			Name:      staff.Name,
+			Mobile:    staff.Mobile,
+			CreatedAt: staff.CreatedAt.Format(constant.CSTLayout),
+			UpdatedAt: staff.UpdatedAt.Format(constant.CSTLayout),
+		}
+		rsp.Data = append(rsp.Data, &staffInfoRsp)
+	}
+
+	return rsp, nil
+}
+
+func (s *StaffService) VerifyStaff(ctx context.Context, req *generate.VerifyStaffRequest) (*generate.VerifyStaffReply, error) {
+	_, err := s.uc.VerifyStaff(ctx, &biz.Staff{
+		Mobile: req.Mobile,
+	})
+	if err != nil {
+		return nil, err
+	}
+	verifyStaffReply := generate.VerifyStaffReply{
+		Exec: true,
+	}
+	return &verifyStaffReply, nil
+}
